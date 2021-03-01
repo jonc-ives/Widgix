72065a3b796c20debea25e7ad69b8ef7844a0a04
Jonathan Ives <jonc.ives@gmail.com>
2021-03-01 16:21:45 -0600

    Final dashboard for this version. Serves widgets. Widget Pane template.

--- application/gui/script/dashboard.js
+++ application/gui/script/dashboard.js
@@ -1,61 +1,59 @@
 const { ipcRenderer } = require('electron');
 
-var change_remote = function() {
-	// assign new_value here
-	ipcRenderer.send('change-remote', new_value);
+var dblogs = {};
+var server = [false, "good"];
+
+var copy_widget_link = function(event) {
+	// code goes here...
 };
 
-var add_trusted = function() {
-	// assign new_trusted here
-	ipcRenderer.send('add-trusted', new_trusted);
-};
-
-var change_port = function() {
-	// assert server is inactive, indicate if not
-	// assign new_port here
-	ipcRenderer.send('change-port', new_port);
-};
-
-var change_refresh = function() {
-	// assign new_refresh here
-	ipcRenderer.send('change-refresh', new_refresh);
-};
-
-var change_debug = function() {
-	// assing new_debug here
-	ipcRenderer.send('change-debug', new_debug);
-};
-
-var preview_widget = function() {
-	// assign widget_name here
+var preview_widget = function(event) {
+	// assign widget_name here...
 	ipcRenderer.send('launch-preview', widget_name);
 };
 
-var create_widget = function() {
+var create_widget = function(event) {
 	// open new widget dialogue launcher
 	// if cancel, return to application
-	// assign widget_dict here
+	// assign widget_dict here...
 	ipcRenderer.send('create-widget', widget_dict);
 };
 
-var copy_widget = function() {
-	// assign widget_name here
+var copy_widget = function(event) {
+	// assign widget_name here...
 	ipcRenderer.send('copy-widget', widget_name);
 };
 
-var open_widget = function() {
-	// assign widget_name here
+var open_widget = function(event) {
+	// assign widget_name here...
 	ipcRenderer.send('edit-widget', widget_name);
 };
 
-var delete_widget = function() {
-	// assign widget_name here
+var delete_widget = function(event) {
+	// assign widget_name here...
 	// launch delete widget window
 	// if cancel, return to application
 	ipcRenderer.send('delete-widget', widget_name);
 }
 
-var toggle_server = function() {
-	// make style changes here
+var toggle_server = function(event) {
+	document.getElementById("server-st").innerHTML = "hold on a moment";
+	document.getElementById("server-st-q").className = "cell cell-on";
 	ipcRenderer.send('toggle-server');
-};
+}; ipcRenderer.on('toggle-server-reply', (event, state) => {
+	document.getElementById("server-st-q").className = "cell cell-off";
+	if (state) {
+		document.getElementById("server-st").innerHTML = "serving widgets";
+		document.getElementById("server-tg").className = "sld-track sld-tr-on";
+		document.getElementById("server-tg-thumb").className = "sld-thumb sld-th-on";
+	} else {
+		document.getElementById("server-st").innerHTML = "ready to serve";
+		document.getElementById("server-tg").className = "sld-track sld-tr-off";
+		document.getElementById("server-tg-thumb").className = "sld-thumb sld-th-off";
+	}
+});
+
+ipcRenderer.on('load-db', (event, data) => {
+	// create widget panes
+	// bind widget-pane actions
+});
--- application/widgix.js
+++ application/widgix.js
@@ -6,105 +6,102 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const express = require('express');
 const fileSystem = require('fs');
 const hbs = require('hbs');
+const path = require('path');
 
 // library imports
 const moduleAPI = require(__dirname + '/lib/module.js');
 
-// here are a states for the app
-var http_instance = undefined;
-var server_on = false;
-var network_on = false;
-var service_port = 3000;
-var log_buffer = [];
-
-// the first thing is validation of dir ./user_data
-if (!fileSystem.existsSync(__dirname + "/user_data"))
-	fileSystem.mkdirSync(__dirname + "/user_data", { recursive: true }, (error) => { if (error) throw error; });
-
-// the next thing is validation of dir ./modules
-if (!fileSystem.existsSync(__dirname + "/lib/modules"))
-	fileSystem.mkdirSync(__dirname + "/lib/modules", { recursive: true }, (error) => { if (error) throw error; })
-
-// assign and validate user_styles dictionary
-let user_styles = __dirname + "/user_data/styles.json";
-if (!fileSystem.existsSync(user_styles))
-	fileSystem.writeFile(user_styles, "{}", (error) => { if (error) throw error; });
-
-// assign and validate user_widgets dictionary
-let user_widgets = __dirname + "/user_data/widgets.json";
-if (!fileSystem.existsSync(user_widgets))
-	fileSystem.writeFile(user_widgets, "{}", (error) => { if (error) throw error; });
-
-// assign and validate user_settings dictionary
-let user_settings = __dirname + "/user_data/styles.json";
-if (!fileSystem.existsSync(user_settings))
-	fileSystem.writeFile(user_settings, "{}", (error) => { if (error) throw error; });
-
-// assign and validate stream_map dictionary
-let stream_map = __dirname + "/lib/modules/map.json";
-if (!fileSystem.existsSync(stream_map))
-	fileSystem.writeFile(stream_map, "{}", (error) => { if (error) throw error; });
-
-let dblogs = JSON.parse(fileSystem.readFileSync(__dirname + "/lib/dblogs.json"));
-if (!dblogs) throw "FATAL -- no console log data. Rebuild application or restore library files.";
-
-// a few filesystem procedures for easier work (all sync)
-var getWidgetObject = function(name) {
-	try {
-		let widget_map = JSON.parse( fileSystem.readFileSync(user_widgets) );
-		let widget = widget_map[name];
-		if (!widget) throw "Widget '" + name + "' does not exist.";
-		return widget;
-	} catch(Error) { console.log(Error); return 0; }
-};
-
-var getRulesObject = function(style_list) {
-	try {
-		let style_rules = JSON.parse( fileSystem.readFileSync(user_styles) );
-		var rules = {}; // declare it as an empty object
-		for (var k=0; k<style_list.length; k++) {
-			var r = style_rules[style_list[k]];
-			if (r && style_list[k] !== "type") rules[style_list[k]] = r;
-		} // that should be the end 
-		return rules;
-	} catch(Error) { console.log(Error); return 0; }
-};
-
-var useModuleAPI = async function(streams, callback) {
-	try {
-		strdat = await moduleAPI.getStreamData(streams);
-		return strdat;
-	} catch(Error) {
-		return 0;
+// default settings if they can't be found
+const configdef = {
+	"widgets": ["/user_data", "relative"],
+	"modules": ["/lib/modules", "relative"],
+	"writeOnMissing": "true",
+	"user_settings": {
+		"server_port": 3000,
+		"serve_remote": "no",
+		"trusted": ["127.000.000.001"],
+		"refresh_rate": "medium",
+		"config": "all"
 	}
-};
-
-// these are all for user notifications and logs
-var buildNewLog = async function(obj) {
-	var time = new Date().toLocaleTimeString();
-	log_buffer.push({
-		"type": obj.type,
-		"text": time + " - " + obj.msg
-	});
 }
 
-var rnameNotify = function() {
-	buildNewLog(dblogs["no-rname"]);
+// here are a few states for the app
+var http_instance = undefined;
+var service_port = 3000;
+var dblog_buffer = [];
+
+// the application content channel -- cannot be created before app.onReady 
+var appWindow = null;
+
+// SYNCHRONOUS FILE SYSTEM AND USER DATA CONFIGURATION
+// Note - if at any point data is written to file, the corresponding object must be updated.
+
+var createDirectory = function(pathname, dblog_key) {
+	pathname = pathname.replace(/^\.*\/|\/?[^\/]+\.[a-z]+|\/$/g, '');
+	fileSystem.mkdirSync(path.resolve(__dirname, pathname), { recursive: true });
 };
 
-var widgetNotify = function() {
-	buildNewLog(dblogs["bad-rname"]);
+var writeFile = function(pathname, data, dblog_key) {
+	pathname = pathname.replace(/^\.*\/|\/$/g, '');
+	fileSystem.writeFileSync(path.resolve(__dirname, pathname), JSON.stringify(data));
 };
 
-var rulesNotify = function() {
-	buildNewLog(dblogs["failed-rules"]);
+var createDirFile = function(pathname, data, dblog_key) {
+	var directory = pathname.replace(/\/?[^\/]+\.[a-z]+|\/$/g, '');
+	if (!fileSystem.existsSync(directory))
+		fileSystem.mkdirSync(path.resolve(directory, { recursive: true }));
+	if (!fileSystem.existsSync(pathname))
+		fileSystem.writeFileSync(path.resolve(__dirname, pathname), JSON.stringify(data));
 };
 
-var strdatNotify = function() {
-	buildNewLog(dblogs["failed-strdat"]);
+var readFile = function(pathname, dblog_key) {
+	var raw = fileSystem.readFileSync(pathname);
+	return JSON.parse(raw);
 };
 
-// create and route express server for application's use
+// locate/create config file
+let config_fn = __dirname + "/config.json";
+if (!fileSystem.existsSync(config_fn))
+	writeFile(config_fn, configdef, "configdne");
+// read config object from config file
+var config_obj = readFile(config_fn, "configobj");
+
+// locate/create widgets file and path
+var widgets_fn = config_obj["widgets"][0] ? config_obj["widgets"][0] : configdef["widgets"][0];
+widgets_fn = (config_obj["widgets"][1] === "absolute" ? "" : __dirname) + widgets_fn + "/widgets.json";
+if (!fileSystem.existsSync(widgets_fn) && config_obj["writeOnMissing"] === "true")
+	createDirFile(widgets_fn, "{}", "widgetsdne")
+else if (!fileSystem.existsSync(widgets_fn))
+	fatal_error("widgetsdne");
+// read widgets object from widgets file
+var widgets_obj = readFile(widgets_fn, "widgetsobj");
+
+// locate/create modules file and path
+var modules_fn = config_obj["modules"][0] ? config_obj["modules"][0] : configdef["modules"][0];
+modules_fn = (config_obj["modules"][1] === "absolute" ? "" : __dirname) + modules_fn + "/modules.json";
+if (!fileSystem.existsSync(modules_fn) && config_obj["writeOnMissing"] === "true")
+	createDirFile(modules_fn, "{}", "modulesdne")
+else if (!fileSystem.existsSync(modules_fn))
+	fatal_error("modulesdne");
+// read widgets object from widgets file
+var modules_obj = readFile(modules_fn, "modulesobj");
+
+// LOGGING, DEBUGGING, CONSOLE MESSAGES
+
+var fatal_error = function(dblog) {
+
+};
+
+var handle_error = function(dblog, error) {
+
+};
+
+var build_dblog = function(dblog) {
+
+};
+
+// CONFIGURE AND MANAGE THE SERVER
+
 const server = express();
 server.set('views', __dirname + "/hbs");
 server.set('view engine', 'hbs');
@@ -121,11 +118,7 @@ server.get('/widget', (req, res) => {
 	// get the widget_object -- this is sync
 	let widget = getWidgetObject(rname);
 	if (widget === 0) return widgetNotify();
-	// get the rules_object -- this is sync
-	let rules = getRulesObject(widget["styles"]);
-	if (rules === 0) return rulesNotify();
 	// get the strdat_object -- this is async so callback to render
-
 	moduleAPI.getStreamData(widget["stream"]).then((strdat) => {
 		if (strdat === 0) return strdatNotify();
 		let render = { // build the render_object for render
@@ -133,90 +126,66 @@ server.get('/widget', (req, res) => {
 		 	"hwidth": widget["width"] ? widget["width"] : "800px",
 		 	"hheight": widget["height"] ? widget["height"] : "600px",
 		 	"refsh": widget["refsh"] ? widget["refsh"] : "300",
-		 	"styles": rules,
 		 	"strdat": strdat,
 		 	"hnodes": widget["hnodes"] ? widget["hnodes"] : {}
 		}; buildNewLog(dblogs["widget-served"]); res.render('main', render);
 	});
 });
 
-server.get('/', (req, res) => {
+// INTERFACE TO RENDERER PROCESS
+
+ipcMain.on('launch-preview', (event, widget_name) => {
 
 });
 
-server.use((req, res) => {
+ipcMain.on('create-widget', (event, widget_dict) => {
 
 });
 
-server.use((error, req, res, next) => {
+ipcMain.on('copy-widget', (event, widget_name) => {
 
 });
 
-// electron's interfaces with the node API
+ipcMain.on('edit-widget', (event, widget_name) => {
 
-ipcMain.on('read-console', (event) => {
-	event.reply('read-console-reply', log_buffer);
-	log_buffer = []; // no race -- all on main
+});
+
+ipcMain.on('rename-widget', (event, new_name) => {
+
+});
+
+ipcMain.on('delete-widget', (event, key) => {
+	
+});
+
+ipcMain.on('save-widget', (event, widget_name) => {
+
 });
 
 ipcMain.on('toggle-server', (event) => {
-	if (!server_on) {
-		http_instance = server.listen({port: service_port}, function(){
-			server_on = true;
-		event.reply('toggle-server-reply', server_on);
+	if (http_instance === undefined) {
+		http_instance = server.listen({ port: service_port }, function(error) {
+			if (error) handle_error("serverup", error);
+			event.reply('toggle-server-reply', (http_instance ? true : false));
 		});
-	} else {
-		http_instance.close(function() {
-			server_on = false;
-			event.reply('toggle-server-reply', server_on);
+	} else if (http_instance) {
+		http_instance.close(function(error) {
+			if (error) handle_error("serverdown", error);
+			else http_instance = undefined;
+			event.reply('toggle-server-reply', (http_instance ? true : false));
 		});
 	}
 });
 
-ipcMain.on('toggle-network', (event) => {
-	network_on = !network_on;
-	event.reply('toggle-network-reply', network_on);
-});
+// BEGIN THE APPLICATION
 
-ipcMain.on('get-widgets', (event) => {
-	var wd_buffer = JSON.parse(fileSystem.readFileSync(user_widgets));
-	event.reply('get-widgets-reply', wd_buffer);
-});
-
-ipcMain.on('delete-widget', (event, key) => {
-	if (!key) return; console.log(key);
-	var old_widgets = JSON.parse(fileSystem.readFileSync(user_widgets));
-	delete old_widgets[key]; var new_widgets = JSON.stringify(old_widgets);
-	console.log(old_widgets); console.log(new_widgets);
-	fileSystem.writeFile(user_widgets, new_widgets, function(err) {
-		if (err) event.reply('delete-widget-reply', false);
-		else event.reply('delete-widget-reply', true);
-	});
-});
-
-ipcMain.on('request-styles', (event) => {
-	let style_rules = JSON.parse( fileSystem.readFileSync(user_styles) );
-	event.reply('request-styles-reply', style_rules);
-});
-
-// all we need to start the gui
 app.whenReady().then(() => {
 	appWindow = new BrowserWindow({
-		title: "Widgix - Custom OBS Browser Sources",
-		width: 200, height: 224, autoHideMenuBar: true,
-		webPreferences: { nodeIntegration: true }
-	}); appWindow.maximize();
-	appWindow.openDevTools();
-	appWindow.loadFile(__dirname + '/gui/index.html');
-	buildNewLog(dblogs["app-loaded"]);
+	title: "Widgix - Custom OBS Browser Sources",
+	width: 900, height: 633, autoHideMenuBar: true,
+	webPreferences: { nodeIntegration: true }
+	});	appWindow.maximize(); appWindow.openDevTools();
+	appWindow.loadFile(`${__dirname}/gui/index.html`);
 });
 
-
-
-// for this side of things
-// ipcMain.handle('perform-action', (event, ...args) => {
-//   // ... do actions on behalf of the Renderer
-// })
-
-// for the browser side scripting
-// ipcRenderer.invoke('perform-action', ...args)
+// appWindow.webContents.postMessage('dashboard-update', data);
