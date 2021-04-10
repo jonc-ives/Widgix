b9c28dec49012b80df1dd6a16a81434fcd99763e
Jonathan Ives <jonc.ives@gmail.com>
2021-04-10 01:57:49 -0500

    Initialized environment for windows installation process

--- widgix.js
+++ widgix.js
@@ -19,6 +19,72 @@ var config = {
 
 const logger = new logs.ApplicationLogManager(__dirname);
 
+// INSTALLATION SETUP
+
+function handleSquirrelEvent(application) {
+    if (process.argv.length === 1) {
+        return false;
+    }
+
+    const ChildProcess = require('child_process');
+    const path = require('path');
+
+    const appFolder = path.resolve(process.execPath, '..');
+    const rootAtomFolder = path.resolve(appFolder, '..');
+    const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+    const exeName = path.basename(process.execPath);
+
+    const spawn = function(command, args) {
+        let spawnedProcess, error;
+
+        try {
+            spawnedProcess = ChildProcess.spawn(command, args, {
+                detached: true
+            });
+        } catch (error) {}
+
+        return spawnedProcess;
+    };
+
+    const spawnUpdate = function(args) {
+        return spawn(updateDotExe, args);
+    };
+
+    const squirrelEvent = process.argv[1];
+    switch (squirrelEvent) {
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            // Optionally do things such as:
+            // - Add your .exe to the PATH
+            // - Write to the registry for things like file associations and
+            //   explorer context menus
+
+            // Install desktop and start menu shortcuts
+            spawnUpdate(['--createShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-uninstall':
+            // Undo anything you did in the --squirrel-install and
+            // --squirrel-updated handlers
+
+            // Remove desktop and start menu shortcuts
+            spawnUpdate(['--removeShortcut', exeName]);
+
+            setTimeout(application.quit, 1000);
+            return true;
+
+        case '--squirrel-obsolete':
+            // This is called on the outgoing version of your app before
+            // we update to the new version - it's the opposite of
+            // --squirrel-updated
+
+            application.quit();
+            return true;
+    }
+};
+
 // LOCATE PERSISTENCE FOLDER -- WRITES ON DNE
 
 fsys.assertFileDirectory(__dirname + "/persistence", true, (error) => {
@@ -115,6 +181,8 @@ const server = new expr.ApplicationServiceManager(config);
 
 const application = main.WidgixProcessManager;
 config["root"] = __dirname;
+
+if (handleSquirrelEvent(application.app)) return;
 
 application.app.whenReady().then( () => {
 	application.start(server, logger, config);
